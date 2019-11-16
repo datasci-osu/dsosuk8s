936490352d250ad12d27e699e3cc2572f8bfb098
Shawn O'Neil <oneilsh@gmail.com>
2019-11-15 22:19:07 -0800

    nfshome dev

---
+++ applications/datascience-notebook/start-notebook.d/mount_home_nfs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+
+if [[ -z $NFS_HOME_SVC ]]; then 
+  echo "NFS /home mount requested from $NFS_HOME_SVC"
+
+  echo "Temporarily relocating /home/jovyan"
+  mv /home/jovyan /tmp
+  echo "Done relocting /home/jovyan to /tmp"
+  
+  echo "Mounting NFS to /home"
+  mount -o soft $NFS_HOME_SVC:/ /home
+  echo "Done mounting NFS to /home"
+
+  echo "Relocting jovyan from /tmp back to /home"
+  mv /tmp/jovyan /home
+ echo "Done relocting from /tmp back to /home"
+fi
+
+
