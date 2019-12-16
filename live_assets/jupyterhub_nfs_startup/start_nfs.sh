04505bd3db6cd76149bbfbbb65f54e603b0fae4d
Shawn O'Neil <oneilsh@gmail.com>
2019-12-16 15:08:15 -0800

    rstudio attempts

--- live_assets/jupyterhub_nfs_startup/start_nfs.sh
+++ live_assets/jupyterhub_nfs_startup/start_nfs.sh
@@ -44,8 +44,15 @@ run-hooks /usr/local/bin/start-notebook.d
 # there must be a reason to do it this way...
 sed -r "s#Defaults\s+secure_path=\"([^\"]+)\"#Defaults secure_path=\"\1:$CONDA_DIR/bin\"#" /etc/sudoers | grep secure_path > /etc/sudoers.d/path
 
+#############################################
+#### For RStudio
+#############################################
+# TODO: I'm not sure I like all this tie-in between the image and the chart (and by extension, this). There's gotta be a better way.
+PATH="${PATH}:/usr/lib/rstudio-server/bin"
+LD_LIBRARY_PATH="/usr/lib/R/lib:/lib:/usr/lib/x86_64-linux-gnu:/usr/lib/jvm/java-7-openjdk-amd64/jre/lib/amd64/server:/opt/conda/lib/R/lib"
+
 echo "Executing the command: ${cmd[@]}"
-exec sudo -E -H -u $NB_USER PATH=$PATH XDG_CACHE_HOME=/home/$NB_USER/.cache PYTHONPATH=${PYTHONPATH:-} "${cmd[@]}"
+exec sudo -E -H -u $NB_USER PATH=$PATH XDG_CACHE_HOME=/home/$NB_USER/.cache PYTHONPATH=${PYTHONPATH:-} LD_LIBRARY_PATH=${LD_LIBRARY_PATH:-} "${cmd[@]}"
 
 
 
