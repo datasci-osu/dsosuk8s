ea71a03dd49b9bb5fc9dbad9b5636dfbee7ed2e4
Shawn O'Neil <oneilsh@gmail.com>
2020-02-12 09:37:51 -0800

    tweaks to stress.R

--- kubernetes_dev/stress.R
+++ kubernetes_dev/stress.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-
+  
 library(stringr)
 library(rstackdeque)
 library(pryr)
@@ -9,20 +9,21 @@ ram_request <- as.integer(max(1, rnorm(1, 256, 64))) # in megs
 
 rows <- rstack()
 
-for(i in 1:rand_iterations) {
+for(i in 1:5) {
   write <- system(paste0("sync; dd if=/dev/zero of=tempfile bs=1M count=256 2>&1 ; sync "), intern = TRUE)
   write_speed <- paste(str_split(write[3], " ")[[1]][c(10, 11)], collapse = " ")
 
   read <- system(paste0("sync; dd if=tempfile of=/dev/null bs=1M count=256 2>&1 ; sync "), intern = TRUE)
   read_speed <- paste(str_split(read[3], " ")[[1]][c(10, 11)], collapse = " ")
-  
+
   user <- system("echo $USER", intern = TRUE)
   current_time <- Sys.time()
   iteration <- i
-  
-  rand_sleep <- max(0, 3 + rexp(1, 0.5))
-  system(paste0("sleep ", rand_sleep))
-  
+
+  #rand_sleep <- max(0, 3 + rexp(1, 0.5))
+  #system(paste0("sleep ", rand_sleep))
+  system("rm tempfile")
+
   gc()
   big_vec <- runif((1000*1000/8)*ram_request)
 
@@ -33,10 +34,10 @@ for(i in 1:rand_iterations) {
                    read_speed = read_speed,
                    ram_request = ram_request,
                    mem_use = as.integer(mem_used() / (1000*1000))) # in megs
-  
+
   rm(big_vec)
   rows <- insert_top(rows, res_list)
 }
 
 write.table(as.data.frame(rows),
-            row.names = FALSE, file = "", quote = FALSE, sep = "\t")
+            row.names = FALSE, file = "res.txt", quote = FALSE, sep = "\t")
