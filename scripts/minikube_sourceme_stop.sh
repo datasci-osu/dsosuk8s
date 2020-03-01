feb632fa71e1330fe1c0732c7a609c14d353d391
Shawn O'Neil <oneilsh@gmail.com>
2020-03-01 10:35:08 -0800

    backshelving help2_setup and minikube scripts

--- scripts/minikube_sourceme_stop.sh
+++
@@ -1,6 +0,0 @@
-echo "Unconfiguring minikube..."
-
-eval $(minikube docker-env -u)
-minikube stop
-
-echo "Minikube unconfigured: minikube VM stopped; docker, kubectl, and helm no longer refer to minikube VM."
