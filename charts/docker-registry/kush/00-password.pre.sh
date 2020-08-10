bafa11fe68a4cbe83a93684b8171701f7aa4214e
Shawn O'Neil <oneilsh@gmail.com>
2020-08-10 10:14:02 -0700

    docker-registry kushed

--- charts/docker-registry/kush/00-password.pre.sh
+++
@@ -1,7 +0,0 @@
-if [ "$ADMIN_USER" == "" ]; then
-  export ADMIN_USER=admin
-fi	
-if [ "$ADMIN_PASSWD" == "" ]; then
-  export ADMIN_PASSWD=$(openssl rand -hex 3)
-fi
-export HTPASSWD_CREDS=$(htpasswd -Bbn $ADMIN_USER $ADMIN_PASSWD)
--- charts/docker-registry/kush/00-showpass.post.sh
+++
@@ -1,4 +0,0 @@
-echo "${green}Done, using htpasswd (basic) authentication.${white}"
-echo "${green}Registry User: $ADMIN_USER${white}"
-echo "${green}Registry Password: $ADMIN_PASSWD${white}"
-
