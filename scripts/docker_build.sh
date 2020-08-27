00c7e71d87d09465b6ef2c5baf4efac944fad223
Shawn O'Neil <oneilsh@gmail.com>
2020-08-27 11:24:28 -0700

    structured docker images and updated docker scripts

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -5,29 +5,24 @@ set -e
 if [[ $# != 1 ]]; then
   echo "Usage: docker_build.sh <directory_containing_dockerfile>"
   echo "Will only build if the image has changed (determined by md5summing the directory, sans the ops folder)"
-  echo "Exits 0 if build not needed, 1 for error, 2 for built"
+  echo "Exits 0 if build not needed or build succeeded, 1 for error"
   exit 1
 fi
 
 TARGETDIR=$1
 
+# determine tag as md5sum of files in directory (not git hash, which updates more frequently than images)
+TAG=`find $TARGETDIR -type f -exec md5sum {} \; | grep -v -E '\.swp$' | sort -k 2 | md5sum | head -c 8`
 
-TAG=`find $TARGETDIR -type f -not -path "*/ops/*" -exec md5sum {} \; | grep -v -E '\.swp$' | sort -k 2 | md5sum | head -c 8`
-# if ops/lasttag.txt doesn't exist, or it's contents are different from TAG, we need to rebuild and push. Otherwise we don't
+IMAGENAME=$(grep '^#IMAGE' $TARGETDIR/Dockerfile | awk '{print $2}')
 
-IMAGENAME=`cat $TARGETDIR/image_name.txt`
+if [ "$IMAGENAME" == "" ]; then
+  echo "This script assumes the Dockerfile contains comments e.g. #IMAGE owner/imagename and #TAG v0.0.1 (multiple tag lines allowed)." 1>&2
+  exit 1
+fi
+
 echo -e "\e[33mChecking build $TARGETDIR, looking for tag $IMAGENAME:$TAG.\e[0m"
 
-
-if [ ! -e $TARGETDIR/ops/build_options.txt ]; then 
-  echo -e "\e[31mError: ops/build_options.txt for $TARGETDIR not found.. \e[0m"
-  exit 1 
-fi
-
-if ! grep -q -E 'build:[[:blank:]+]true' $TARGETDIR/ops/build_options.txt; then
-  echo -e "\e[31mbuild: true not set in ops/build_options.txt for $TARGETDIR, skipping. \e[0m"
-  exit 0
-fi
 
 if ! grep -q "$TAG" <(docker image ls); then
   echo -e "\e[32mBuilding $IMAGENAME:$TAG.\e[0m"
@@ -38,7 +33,7 @@ if ! grep -q "$TAG" <(docker image ls); then
   done
   docker tag $IMAGENAME:$TAG $IMAGENAME:latest
   echo -e "\e[32mBuilt $IMAGENAME:$TAG.\e[0m"
-  exit 2
+  exit 0
 else
   echo -e "\e[31m$TARGETDIR present, not building. \e[0m"
   exit 0
--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -7,7 +7,7 @@ set -e
 if [[ ( $# != 1 ) && ( $# != 2 ) ]]; then
   echo "Usage: docker_push.sh <directory_containing_dockerfile>"
   echo "Will only push if the image tag (determined by md5summing the directory, sans the ops folder) is not present."
-  echo "Exits 1 for error, 2 for pushed, 0 for push not needed."
+  echo "Exits 1 for error, 0 for push not needed or successful push."
   exit 1
 fi
 
@@ -17,27 +17,25 @@ if ! grep -q -E '"experimental"[[:blank:]]*:[[:blank:]]*"enabled"' ~/.docker/con
 fi
 
 TARGETDIR=$1
-TAG=`find $TARGETDIR -type f -not -path "*/ops/*" -exec md5sum {} \; | sort -k 2 | md5sum | head -c 8`
-# if ops/lasttag.txt doesn't exist, or it's contents are different from TAG, we need to rebuild and push. Otherwise we don't
-IMAGENAME=`cat $TARGETDIR/image_name.txt`
+# determine tag as md5sum of files in directory (not git hash, which updates more frequently than images)
+TAG=`find $TARGETDIR -type f -exec md5sum {} \; | grep -v -E '\.swp$' | sort -k 2 | md5sum | head -c 8`
 
-echo -e "\e[33mChecking push $TARGETDIR, looking for $IMAGENAME:$TAG.\e[0m"
+IMAGENAME=$(grep '^#IMAGE' $TARGETDIR/Dockerfile | awk '{print $2}')
 
-if [ ! -e $TARGETDIR/ops/build_options.txt ]; then 
-  echo -e "\e[31mError: ops/build_options.txt for $TARGETDIR not found.. \e[0m"
+if [ "$IMAGENAME" == "" ]; then
+  echo "This script assumes the Dockerfile contains comments e.g. #IMAGE owner/imagename and #TAG v0.0.1 (multiple tag lines allowed)." 1>&2
   exit 1
 fi
 
-if ! grep -q -E 'push:[[:blank:]+]true' $TARGETDIR/ops/build_options.txt; then
-  echo -e "\e[31mbuild: push not set in ops/build_options.txt for $TARGETDIR, skipping. \e[0m"
-  exit 0
-fi
+
+echo -e "\e[33mChecking push $TARGETDIR, looking for $IMAGENAME:$TAG.\e[0m"
+
 
 # check for image:tag on dockerhub with docker manifest inspect
 if ! docker manifest inspect $IMAGENAME:$TAG > /dev/null 2> /dev/null; then
   docker push $IMAGENAME | cat       # be quiet about it, sheesh; edit, why doesn't this work per https://github.com/moby/moby/issues/37417#issuecomment-403833610 
   echo -e "\e[32mPushed $IMAGENAME. \e[0m"
-  exit 2
+  exit 0
 else
   echo -e "\e[31m$TARGETDIR already present, not pushing. \e[0m"
   exit 0
