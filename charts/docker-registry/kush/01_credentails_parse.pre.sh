aef075f889828a22b616b3b97b5aa723bdfa574c
Shawn O'Neil <oneilsh@gmail.com>
2020-09-10 10:52:02 -0700

    updated docker-registry chart to support no-auth local nodeport registry

--- charts/docker-registry/kush/01_credentails_parse.pre.sh
+++ charts/docker-registry/kush/01_credentails_parse.pre.sh
@@ -1,10 +1,10 @@
 
-ADMIN_USERNAME=$(index "basicAuth.username" "")
-ADMIN_PASSWORD=$(index "basicAuth.password" "")
+ADMIN_USERNAME=$(index "basicAuth.username" "$ADMIN_USERNAME")
+ADMIN_PASSWORD=$(index "basicAuth.password" "$ADMIN_PASSWORD")
 
-if [ "$ADMIN_USERNAME" == "" ] || [ "$ADMIN_PASSWORD" == "" ]; then
-  echo "${red}Error: you must define both basicAuth.username and basicAuth.password in --values.${white}" 1>&2
-  exit 1
-fi
+#if [ "$ADMIN_USERNAME" == "" ] || [ "$ADMIN_PASSWORD" == "" ]; then
+#  echo "${red}Error: you must define both basicAuth.username and basicAuth.password in --values.${white}" 1>&2
+#  exit 1
+#fi
 
 
