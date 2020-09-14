b3b588c155a3b19f41fbb68f635703a7600b0419
Shawn O'Neil <oneilsh@gmail.com>
2020-09-14 14:01:33 -0700

    fix post-install display for ds-jupyterlab kush chart

--- charts/ds-jupyterlab/kush/00_infodisplay.post.sh
+++ charts/ds-jupyterlab/kush/00_infodisplay.post.sh
@@ -1,4 +1,15 @@
 
-echo "${green}Finished! Hub launch URL:${blue} https://$(index clusterHostname "whoops")/${RELEASE_NAME}/hub/lti/launch${white}" 1>&2
-echo "${green}Consumer Key: ${blue}$LTI_CLIENT_KEY${white}" 1>&2
-echo "${green}Shared Secret: ${blue}$LTI_CLIENT_SECRET${white}" 1>&2
+AUTH_TYPE=$(index authType 'lti')
+ADMIN_USERS=$(index adminUsers '')
+if [ "$AUTH_TYPE" == "lti" ]; then
+  echo "${green}Hub launch URL:${cyan} https://$(index clusterHostname "whoops")/${RELEASE_NAME}/hub/lti/launch${white}" 1>&2
+  echo "${green}Consumer Key: ${cyan}$LTI_CLIENT_KEY${white}" 1>&2
+  echo "${green}Shared Secret: ${cyan}$LTI_CLIENT_SECRET${white}" 1>&2
+else 
+  echo "${green}Auth type is ${cyan}$AUTH_TYPE${white}" 1>&2
+  echo "${green}Admin users: ${cyan}$ADMIN_USERS${white}" 1>&2
+fi
+
+if [ "$DRY_RUN" != "" ]; then
+  echo "${yellow}This is a *dry run* (using helm template or --dry-run), no install performed.${white}" 1>&2
+fi
